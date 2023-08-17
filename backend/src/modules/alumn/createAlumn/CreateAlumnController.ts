8551d566b308190c8ec6c4e818abe03d4ef2d9ba
PedroMartiniano <110617020+PedroMartiniano@users.noreply.github.com>
2023-08-17 20:13:18 -0300

    implementing zod

--- backend/src/modules/alumn/createAlumn/CreateAlumnController.ts
+++ backend/src/modules/alumn/createAlumn/CreateAlumnController.ts
@@ -3,45 +3,64 @@ import { AppError } from "../../../error/AppError";
 import { CreateAlumnUseCase } from "./CreateAlumnUseCase";
 import { GetAlumnByCpfUseCase } from "../getAlumnByCpf/GetAlumnByCpfUseCase";
 import { CreateUserAlumnUseCase } from "../../users/createUser/CreateUserAlumnUseCase";
+import { z } from "zod";
 
 export class CreateAlumnController {
     async handle(req: Request, res: Response) {
-        const { first_name, last_name, cpf, email, password } = req.body
 
-        if (!first_name || typeof first_name !== "string") {
+        const alumnSchema = z.object({
+            first_name: z.string(),
+            last_name: z.string(),
+            cpf: z.string().length(11),
+            email: z.string().email(),
+            password: z.string().min(6)
+        })
+
+        const alumnData = alumnSchema.safeParse(req.body)
+
+        if(!alumnData.success) {
             return res.status(400).json({
                 success: false,
-                message: "missing first_name body param"
+                message: alumnData.error
             })
         }
 
-        if (!last_name || typeof last_name !== "string") {
-            return res.status(400).json({
-                success: false,
-                message: "missing last_name body param"
-            })
-        }
+        const { first_name, last_name, cpf, email, password } = alumnData.data
 
-        if (!cpf || typeof cpf !== "string") {
-            return res.status(400).json({
-                success: false,
-                message: "missing cpf body param"
-            })
-        }
+        // if (!first_name || typeof first_name !== "string") {
+        //     return res.status(400).json({
+        //         success: false,
+        //         message: "missing first_name body param"
+        //     })
+        // }
 
-        if (!email || typeof email !== "string") {
-            return res.status(400).json({
-                success: false,
-                message: "missing email body param"
-            })
-        }
+        // if (!last_name || typeof last_name !== "string") {
+        //     return res.status(400).json({
+        //         success: false,
+        //         message: "missing last_name body param"
+        //     })
+        // }
 
-        if (!password || typeof password !== "string") {
-            return res.status(400).json({
-                success: false,
-                message: "missing password body param"
-            })
-        }
+        // if (!cpf || typeof cpf !== "string") {
+        //     return res.status(400).json({
+        //         success: false,
+        //         message: "missing cpf body param"
+        //     })
+        // }
+
+        // if (!email || typeof email !== "string") {
+        //     return res.status(400).json({
+        //         success: false,
+        //         message: "missing email body param"
+        //     })
+        // }
+
+        // if (!password || typeof password !== "string") {
+        //     return res.status(400).json({
+        //         success: false,
+        //         message: "missing password body param"
+        //     })
+        // }
 
         const getAlumnByCpf = new GetAlumnByCpfUseCase
         const alumn = await getAlumnByCpf.execute(cpf)
